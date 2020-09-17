8658775e381db6000ad84eb9d9b3be8b4204a06e
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-09-17 14:16:55 -0700

    1st pass at single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,38 +5,27 @@ Returns: an integer
 
 
 def single_number(arr):
-    # Thoughts
-    # The array is never empty
-    # Every element is repeated, except for one
+    # list of non repeated numbers
+    non_repeated_numbers = []
 
-    # How I understand the problem
-    # I need some way to inspect every element in the array
-    # Since I know the array will never be empty, I don't need to confirm that before inspecting the elements
-    # Every time I run through the numbers, I need a way to keep track of the current value I'm checking against
-    # I also need some way to confirm if the current value was found again or not
-    # Finally, I need to return the only value that I didn't confirm as repeated
+    # loop through every number in the list
+    for num in arr:
+        # check whether num is inside non_repeated_numbers list, they won't first time arround
+        if num in non_repeated_numbers:
+            # if number is there, that means it's repeated, so remove it from the list
+            non_repeated_numbers.remove(num)
+        else:
+            # if number is not there, make sure to add it
+            non_repeated_numbers.append(num)
 
-    # test_list = [4, 1, 2, 1, 2]
-    # if arr in test_list:
-    #     print(True)
-    # else:
-    #     print(False)
-    # for index, num in arr:
-    #     repeated = []
-    #     if
+    # grab the only value that's left from non_repeated_numbers
+    result = non_repeated_numbers[0]
 
-    # for num1 in range(len(arr)-1):
-    #     for num2 in range(num1 + 1, len(arr)):
-    #         found = 0
-    #         if arr[num1] == arr[num2]:
-    #             found += 1
-    #             print(found)
+    return result
 
-    # if __name__ == '__main__':
-    #     # Use the main function to test your implementation
-    #     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    #     print(f"The odd-number-out is {single_number(arr)}")
+if __name__ == '__main__':
+    # Use the main function to test your implementation
+    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    # single_number([4, 1, 2, 1, 2])
-single_number(10)
+    print(f"The odd-number-out is {single_number(arr)}")
